6f4daa73f9badfe63af8eda9bfa72689bdaba3b1
Simone Russo <34507615+simonerusso97@users.noreply.github.com>
2021-10-07 22:49:15 +0200

    Working
    Here is working both publish and subscribe (send and receive messages)

--- app/src/main/java/it/unisalento/mqtt_test/MainActivity.java
+++ app/src/main/java/it/unisalento/mqtt_test/MainActivity.java
@@ -3,12 +3,66 @@ package it.unisalento.mqtt_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import it.unisalento.mqtt_test.helpers.MQTTHelper;
 
 public class MainActivity extends AppCompatActivity {
+    MQTTHelper mqttHelper;
 
+    TextView broker;
+    TextView dataReceived;
+    Button publishButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataReceived = (TextView) findViewById(R.id.dataReceived);
+        broker = (TextView) findViewById(R.id.broker);
+        publishButton = (Button) findViewById(R.id.publishButton);
+
+        publishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mqttHelper.send("Invio un messaggio");
+            }
+        });
+        startMqtt();
+    }
+
+    private void startMqtt(){
+        mqttHelper = new MQTTHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                broker.setText(s.toString());
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+                dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+
     }
 }
